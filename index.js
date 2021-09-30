8509dacef8fb0dc866988e07f2532a66e458e189
Jimmy Wärting <jimmy@warting.se>
2021-09-30 13:44:34 +0200

    switch to class

--- index.js
+++ index.js
@@ -1,54 +1,65 @@
 var stream = require('readable-stream')
-var inherits = require('inherits')
 
-var SIGNAL_FLUSH =(Buffer.from && Buffer.from !== Uint8Array.from)
-  ? Buffer.from([0])
-  : new Buffer([0])
+var SIGNAL_FLUSH = Buffer.from([0])
 
-module.exports = WriteStream
+class WriteStream extends stream.Writable {
+  constructor (opts, write, flush) {
+    if (typeof opts === 'function') {
+      flush = write
+      write = opts
+      opts = {}
+    }
 
-function WriteStream (opts, write, flush) {
-  if (!(this instanceof WriteStream)) return new WriteStream(opts, write, flush)
+    super(opts)
 
-  if (typeof opts === 'function') {
-    flush = write
-    write = opts
-    opts = {}
+    this.destroyed = false
+    this._worker = write || null
+    this._flush = flush || null
   }
 
-  stream.Writable.call(this, opts)
+  _write(data, enc, cb) {
+    if (SIGNAL_FLUSH === data)
+      this._flush(cb)
+    else
+      this._worker(data, enc, cb)
+  }
 
-  this.destroyed = false
-  this._worker = write || null
-  this._flush = flush || null
+  end(data, enc, cb) {
+    if (!this._flush)
+      return stream.Writable.prototype.end.apply(this, arguments)
+    if (typeof data === 'function')
+      return this.end(null, null, data)
+    if (typeof enc === 'function')
+      return this.end(data, null, enc)
+    if (data)
+      this.write(data)
+    if (!this._writableState.ending)
+      this.write(SIGNAL_FLUSH)
+    return stream.Writable.prototype.end.call(this, cb)
+  }
+
+  destroy(err) {
+    if (this.destroyed)
+      return
+    this.destroyed = true
+    if (err)
+      this.emit('error', err)
+    this.emit('close')
+  }
+
+  static obj(opts, worker, flush) {
+    if (typeof opts === 'function')
+      return WriteStream.obj(null, opts, worker)
+    if (!opts)
+      opts = {}
+    opts.objectMode = true
+    return new WriteStream(opts, worker, flush)
+  }
 }
 
-inherits(WriteStream, stream.Writable)
-
-WriteStream.obj = function (opts, worker, flush) {
-  if (typeof opts === 'function') return WriteStream.obj(null, opts, worker)
-  if (!opts) opts = {}
-  opts.objectMode = true
-  return new WriteStream(opts, worker, flush)
-}
-
-WriteStream.prototype._write = function (data, enc, cb) {
-  if (SIGNAL_FLUSH === data) this._flush(cb)
-  else this._worker(data, enc, cb)
-}
-
-WriteStream.prototype.end = function (data, enc, cb) {
-  if (!this._flush) return stream.Writable.prototype.end.apply(this, arguments)
-  if (typeof data === 'function') return this.end(null, null, data)
-  if (typeof enc === 'function') return this.end(data, null, enc)
-  if (data) this.write(data)
-  if (!this._writableState.ending) this.write(SIGNAL_FLUSH)
-  return stream.Writable.prototype.end.call(this, cb)
-}
-
-WriteStream.prototype.destroy = function (err) {
-  if (this.destroyed) return
-  this.destroyed = true
-  if (err) this.emit('error', err)
-  this.emit('close')
-}
+// To be backwards compatible without calling `new`
+module.exports = new Proxy(WriteStream, {
+  apply (target, thisArg, argumentsList) {
+    return new target(...argumentsList)
+  }
+})
